2f528ba06b28e20c79cf90f91e822d301771c06a
Tiago Paz Lino <tiagoplino@gmail.com>
2020-02-13 00:42:15 -0300

    Adicionadas funcionalidades de inserção, deleção e atualização de clientes

--- src/main/java/br/com/tokiomarine/controller/ClienteController.java
+++ src/main/java/br/com/tokiomarine/controller/ClienteController.java
@@ -1,23 +1,38 @@
 package br.com.tokiomarine.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tokiomarine.damain.Cliente;
-import br.com.tokiomarine.repository.ClienteRepository;
+import br.com.tokiomarine.service.ClienteService;
 
 @RestController
-@RequestMapping("clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 	@Autowired
-	private ClienteRepository clienteRepository;
+	private ClienteService clienteService;
 	
-	@GetMapping()
+	@GetMapping
 	public Iterable<Cliente> listarClientes() {
-		return clienteRepository.findAll();
+		return clienteService.findAll();
 	}
 	
+	@PostMapping
+	@PutMapping
+	public Cliente salvarCliente(@RequestBody Cliente cliente){
+		return clienteService.save(cliente);
+	}
+	
+	@DeleteMapping
+	public Cliente deletarCliente(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+
 }
